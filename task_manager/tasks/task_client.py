eb769ac77f1b6c81022916b189235c3bf50555d8
AmitShukrun <amitshukrun3@gmail.com>
2024-10-31 01:47:23 +0200

    - Clean code

--- task_manager/tasks/task_client.py
+++ task_manager/tasks/task_client.py
@@ -4,10 +4,10 @@ import requests
 def run_task_client():
     base_url = "http://localhost:8000/tasks/"
 
-    # # Create new task
-    # task_data = {"title": "Example Task", "description": "This is an example task.", "completed": False}
-    # response = requests.post(f"{base_url}create/", json=task_data)
-    # print("Create Task:", response.json())
+    # Create new task
+    task_data = {"title": "Example Task", "description": "This is an example task.", "completed": False}
+    response = requests.post(f"{base_url}create/", json=task_data)
+    print("Create Task:", response.json())
 
     # # Get all tasks
     # response = requests.get(base_url)
@@ -20,9 +20,9 @@ def run_task_client():
     # response = requests.put(f"{base_url}{task_id}/", json=updated_task_data)
     # print("Update Task:", response.json())
 
-    # Delete Task
-    response = requests.delete(f"{base_url}{task_id}/")
-    print("Delete Task:", response.status_code)
+    # # Delete Task
+    # response = requests.delete(f"{base_url}{task_id}/")
+    # print("Delete Task:", response.status_code)
 
 
 if __name__ == "__main__":
